209696f2b1d0be347d857c017471ddc762df463d
Abi Chang <abi@cloud-latitude.com>
2021-08-29 11:02:47 +0800

    init project

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 緯雲 - 持續重構與TDD工作坊
+
+[Problem](https://sites.google.com/site/tddproblems/all-problems-1/Console-interaction)
---
+++ src/main/java/com/cl/abi/shapecalculator/ShapeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.cl.abi.shapecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapeCalculatorApplication.class, args);
+    }
+
+}
